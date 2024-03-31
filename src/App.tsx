a075f3e3a495b34e129fbac81ed0f940ceb0ee41
albertjie-owner <albertjie-owner>
2024-04-01 00:26:35 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import styles from "./App.module.less";
 function App() {
   return (
     <div className={styles.app}>
+      <div style={{ fontSize: "30px" }}>测试</div>
       <Bpmn></Bpmn>
     </div>
   );
